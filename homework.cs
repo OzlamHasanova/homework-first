571d04ba704dd1e18f67652f031b773739850445
OzlamHasanova <7qk9bme@code.edu.az>
2022-11-16 01:21:32 +0400

    homework-first

---
+++ homework.cs
@@ -0,0 +1,28 @@
+﻿namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            //int a = Convert.ToInt32(Console.ReadLine());
+           
+            //readline-dan aldigimiz deyeri arrayin uzunlugu ile elaqelendire bilmedim.
+            //xeta olaraq sabit deyer girmeli oldugumuzu bildirdi.
+
+
+            int[] array = new int[6] {1,2,3,4,5,9};
+            int sum = 0;
+          
+            foreach (int i in array)
+            {
+                if (i % 2 == 0)
+                {
+                    sum += i;
+                   
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
